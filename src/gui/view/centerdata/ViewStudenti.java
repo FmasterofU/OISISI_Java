45f511e4f3e25473577df0b321613e10c3b9269d
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2019-12-21 23:52:55 +0100

    Setting up basic principle to sorting lists are to be sorted (not table rows) in order to preserve direct mapping between table and lists so that methods of IAbstractTableModel interface always work correctly

--- src/gui/view/centerdata/ViewStudenti.java
+++ src/gui/view/centerdata/ViewStudenti.java
@@ -1,5 +1,8 @@
 package gui.view.centerdata;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import gui.model.ListaStudenata;
 import persistence.Data;
 
@@ -16,6 +19,39 @@ public class ViewStudenti extends ViewTableCenter {
 		model = new ThisTableModel<ListaStudenata>(Data.data.listaStudenata);
 		table.setModel(model);
 		resizeColumnWidth();
+		table.getTableHeader().addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int col = table.columnAtPoint(e.getPoint());
+				System.out.println(col);
+				
+			}
+		});
 	}
 	
 	public void updateTable()
