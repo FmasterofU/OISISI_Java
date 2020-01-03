0a463b7895c839038a1df8dc01d478037bb9a509
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2020-01-03 22:07:15 +0100

    Javadoc documentation Since this is never goning to be a huge repo/project, i think it's more then ok to put the documentation together with source code in a git repo

--- src/rs/ac/uns/ftn/ssluzba/Main.java
+++ src/rs/ac/uns/ftn/ssluzba/Main.java
@@ -5,6 +5,10 @@ import rs.ac.uns.ftn.ssluzba.gui.view.MainWindow;
 
 public class Main {
 
+	/**
+	 * Practically doesn't do a single damn thing :)
+	 * @param args discarded
+	 */
 	public static void main(String[] args) {
 		Data.init();
 		MainWindow.getInstance().setVisible(true);
