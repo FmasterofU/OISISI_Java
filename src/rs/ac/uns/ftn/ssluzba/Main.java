b7fae2a6b257e5a460f26dbfa4015a3fa632d16b
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2020-01-07 21:18:23 +0100

    javadoc

--- src/rs/ac/uns/ftn/ssluzba/Main.java
+++ src/rs/ac/uns/ftn/ssluzba/Main.java
@@ -5,18 +5,18 @@ import rs.ac.uns.ftn.ssluzba.gui.view.MainWindow;
 
 /**
  * @author fmaster
- * Starting point
+ * @implNote Starting point
  */
 public class Main {
 
-	/**
-	 * Practically doesn't do a single damn thing :)
-	 * Well it does, Data initialization and starts up the MainWindow, version number
-	 * @param args discarded
-	 */
 	
 	public static String version = "v1.0.0";
 	
+	/**
+	 * @implNote Practically doesn't do a single damn thing :)
+	 * Well it does, Data initialization and starts up the MainWindow, version number
+	 * @param args discarded
+	 */
 	public static void main(String[] args) {
 		Data.init();
 		MainWindow.getInstance().setVisible(true);
