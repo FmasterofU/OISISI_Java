78bab23f6f0bde0eacbc7e085afb0bac74afca98
Igor Šikuljak <igorsikuljak@uns.ac.rs>
2020-01-11 15:23:57 +0100

    proper date sorting

--- src/rs/ac/uns/ftn/ssluzba/gui/view/centerdata/ViewProfesori.java
+++ src/rs/ac/uns/ftn/ssluzba/gui/view/centerdata/ViewProfesori.java
@@ -1,5 +1,9 @@
 package rs.ac.uns.ftn.ssluzba.gui.view.centerdata;
 
+import java.util.Comparator;
+
+import javax.swing.DefaultRowSorter;
+
 import rs.ac.uns.ftn.ssluzba.gui.controller.Data;
 import rs.ac.uns.ftn.ssluzba.gui.model.ListaProfesora;
 import rs.ac.uns.ftn.ssluzba.gui.view.CenterBox;
@@ -16,15 +20,31 @@ public class ViewProfesori extends ViewTableCenter {
 	private static final int KEY_COLUMN = 7; //kolona licne karte
 	public static boolean inSearchMode = false;
 	public static final int[] SEARCH_COLUMNS = {0, 1, 5, 7};
+	public static final int[] DATE_COLUMNS = {2};
 	
 	public static ViewProfesori getInstance() {
 		if(instance==null) instance = new ViewProfesori();
 		return instance;
 	}
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	private ViewProfesori() {
 		model = new ThisTableModel<ListaProfesora>(Data.getListaProfesora());
 		table.setModel(model);
 		resizeColumnWidth();
+		for(int i : DATE_COLUMNS)
+			((DefaultRowSorter) model.getSorter()).setComparator(i, new Comparator<String>() {
+
+				@Override
+				public int compare(String o1, String o2) {
+					String[] first = o1.split("\\.");
+					String[] second = o2.split("\\.");
+					int[] check = {2, 1, 0};
+					for(int i : check)
+						if(first[i].compareTo(second[i])!=0)
+							return first[i].compareTo(second[i]);
+					return 0;
+				}
+			});
 		table.setRowSorter(model.getSorter());
 	}
 
--- src/rs/ac/uns/ftn/ssluzba/gui/view/centerdata/ViewStudenti.java
+++ src/rs/ac/uns/ftn/ssluzba/gui/view/centerdata/ViewStudenti.java
@@ -1,5 +1,8 @@
 package rs.ac.uns.ftn.ssluzba.gui.view.centerdata;
 
+import java.util.Comparator;
+
+import javax.swing.DefaultRowSorter;
 import rs.ac.uns.ftn.ssluzba.gui.controller.Data;
 import rs.ac.uns.ftn.ssluzba.gui.model.ListaStudenata;
 import rs.ac.uns.ftn.ssluzba.gui.view.CenterBox;
@@ -16,15 +19,32 @@ public class ViewStudenti extends ViewTableCenter {
 	private static final int KEY_COLUMN = 0;
 	public static boolean inSearchMode = false;
 	public static final int[] SEARCH_COLUMNS = {1, 2, 0, 9};
+	public static final int[] DATE_COLUMNS = {3, 10};
 	
 	public static ViewStudenti getInstance() {
 		if(instance == null)	instance = new ViewStudenti();
 		return instance;
 	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private ViewStudenti() {
 		model = new ThisTableModel<ListaStudenata>(Data.getListaStudenata());
 		table.setModel(model);
 		resizeColumnWidth();
+		for(int i : DATE_COLUMNS)
+			((DefaultRowSorter) model.getSorter()).setComparator(i, new Comparator<String>() {
+
+				@Override
+				public int compare(String o1, String o2) {
+					String[] first = o1.split("\\.");
+					String[] second = o2.split("\\.");
+					int[] check = {2, 1, 0};
+					for(int i : check)
+						if(first[i].compareTo(second[i])!=0)
+							return first[i].compareTo(second[i]);
+					return 0;
+				}
+			});
 		table.setRowSorter(model.getSorter());
 		/*
 		table.getTableHeader().addMouseListener(new MouseListener() {
